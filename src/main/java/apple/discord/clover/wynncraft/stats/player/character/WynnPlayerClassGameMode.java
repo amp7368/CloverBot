5a2ddfa9367930026dff68fd5e6cb41d08b36398
Aaron Peterham <47309189+amp7368@users.noreply.github.com>
2023-12-01 17:32:17 -0500

    removed old unused code from v3 update

--- src/main/java/apple/discord/clover/wynncraft/stats/player/character/WynnPlayerClassGameMode.java
+++
@@ -1,9 +0,0 @@
-package apple.discord.clover.wynncraft.stats.player.character;
-
-public class WynnPlayerClassGameMode {
-
-    public boolean craftsman;
-    public boolean hardcore;
-    public boolean ironman;
-    public boolean hunted;
-}
--- src/main/java/apple/discord/clover/wynncraft/stats/player/character/quest/WynnPlayerQuests.java
+++
@@ -1,7 +0,0 @@
-package apple.discord.clover.wynncraft.stats.player.character.quest;
-
-public class WynnPlayerQuests {
-
-    public int completed;
-    public String[] list;
-}
